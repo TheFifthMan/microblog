4cc7894343294f8f84e64e39ab37c4ece622f290
1amb3 <a403481704@gmail.com>
2018-09-28 21:46:33 +0800

    add register

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,ValidationError,Length,EqualTo,Email
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +8,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('password',validators=[DataRequired()])
     remember_me = BooleanField('remember me')
     submit = SubmitField('submit')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('username',validators=[ DataRequired() ])
+    password = PasswordField('password',validators=[DataRequired(),Length(8,20)])
+    password2 = PasswordField('password2',validators=[DataRequired(),EqualTo("password")])
+    email = StringField('email',validators=[DataRequired(),Email()])
+    submit = SubmitField('submit')
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('user exists.')
+
+
+    def validate_email(self,email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('email exists')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template,redirect,url_for,flash
-from  .forms import LoginForm
-from flask_login import login_user,logout_user
+from  .forms import LoginForm,RegisterForm
+from flask_login import login_user,logout_user,login_required,current_user
 from werkzeug.urls import url_parse
 
 
@@ -22,8 +22,18 @@ def index():
     return render_template('index.html',title="Home", user=user,posts=posts)
 
 
+@app.route('/logut')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
 @app.route('/login',methods=['GET',"POST"])
 def login():
+    if current_user.is_authenticated:
+        flash("You have login")
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         #flash("Login requested for user {}, remember me = {}".format(form.username.data,form.remember_me.data))
@@ -43,3 +53,22 @@ def login():
         return redirect(url_for(next_page))
 
     return render_template('login.html',title='login',form=form)
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        flash('You have login')
+        return redirect(url_for('index'))
+    
+    form = RegisterForm()
+    if form.validate_on_submit():
+        u = User(username=form.username.date,email=form.email.data)
+        u.set_password(form.password.data)
+        db.session.add(u)
+        db.session.commit()
+        flash('Conguration! You have the part of this site.')
+        return redirect(url_for('login'))
+
+    return render_template('register.html',title='register',form=form)
+
