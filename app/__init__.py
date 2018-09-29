3f1bc5b470328b9ba8bb23322bb77b433cd35594
1amb3 <a403481704@gmail.com>
2018-09-29 22:55:44 +0800

    add email logs

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,4 +15,22 @@ login_manager.login_view = 'login'
 login_manager.session_protection = 'strong'
 
 
-from app import routes,models
+# 相关的东西需要引入进来，实际就是一个单文件
+from app import routes,models,errors
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'],app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_SSL']:
+            secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost=app.config['MAIL_SERVER'],
+            fromaddr=app.config['MAIL_USERNAME'],
+            toaddrs=app.config['ADMINS'],
+            subject="Microblog Error",
+            credentials=auth,secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
--- config.py
+++ config.py
@@ -4,4 +4,13 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "mysql+pymysql://root:qwe123@127.0.0.1/microblog"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('TOKEN')
+    ADMINS = [os.environ.get('ADMIN_LIST')]
+
+
+
 
